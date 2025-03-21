a21e5bb18ba9e8c6b4f047f6188d79c499c48dc7
Marc Whitbread <marc@rupt.dev>
2025-03-21 11:10:55 -0400

    first cut

--- src/operations/prompt.ts
+++ src/operations/prompt.ts
@@ -13,6 +13,10 @@ export async function Create(
   const response = await ashraRequest(`https://api.ashra.ai/prompt`, {
     method: "POST",
     body: { url, prompt, schema, options },
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer 725d7775c201edf2b9d92f09a0ae2d8592f63ed5c6b40e0cc283b5e520f3af2ba6e8f512cabd5a6841cef4c1ce6ee93a`,
+    },
   });
   return CreatePromptResponseSchema.parse(response);
 }
